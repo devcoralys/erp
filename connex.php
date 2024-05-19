2c1e2d2465e5a701165bcb70692ded8a6583f20d
Ulrich AMANI <amani_ulrich@outlook.fr>
2024-05-19 12:49:51 +0000

    Nouvelles données de connexion

--- connex.php
+++ connex.php
@@ -1,7 +1,7 @@
 <?php 
 try
 {
-$con = new PDO('mysql:host=localhost;dbname=sttci_app_db_2024', 'sttci_ulrich', '@Succes2019');
+$con = new PDO('mysql:host=localhost;dbname=coralys1_erp_db', 'coralys1_admin_dev', '@Coralys2024');
 //$con = new PDO('mysql:host=localhost; dbname=mtransit_db', 'root', '');
 }
 catch(Exception $e)
